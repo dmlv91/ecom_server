bf114ef92636aec5c874b764fe7643e73ae401a9
dmlv <davismedins@gmail.com>
2024-01-07 01:10:18 +0200

    Debug

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@
 
  if(process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
-
+   
     app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'));
  }
 
  const port = process.env.PORT || 5000;
-
+ console.log(__dirname + '/public/index.html');
  app.listen(port, () => console.log(`Server running on ${port}`));
